80a5184f76a12abcfdbd06afbf9aedaa8e966eb6
Erik Norman <erik.norman@caligo.cloud>
2025-03-19 16:51:48 +0100

    export aggregateData

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { extractTags } from './tags/tagExtractor';
 export { transformLine } from './transform/transformLine';
+export { aggregateData } from './aggregate/aggregateData';
 export { FocusLine } from './interfaces/FocusLine';
 export { TransformLineParams } from './interfaces/TransformLineParams';
+export { AggregateDataParams } from './interfaces/AggregateDataParams';
